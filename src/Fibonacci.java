c57dfcf5b8b6e67cbd981158012dc65aa624e03f
Patricia Ortiz Tellez <ot.patricia1@gmail.com>
2023-07-31 01:14:21 -0600

    Se agregaron cambios

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -10,7 +10,7 @@
 	    System.out.println("Ingresa un número:");
 	    
 		int n= in.nextInt();
-		int fibonacci=n , suma=0 , suma1 = 1, cont=0; //El valor n es un valor variable/ al azar que vamos a escoger de la tabla de Fibonacci, suma =0 pq es un valor fijo q se debe iniciar con ese numero, suma=1 tambien es un valor fijo, cont=0 valor fijo porque debe empezar a fuerzas a contar desde 0
+		int fibonacci=n , suma=0 , suma1 = 1, cont=0; //El valor n es una variable que vamos a escoger al azar de la tabla de Fibonacci, suma =0 pq es un valor fijo q se debe iniciar con ese numero, suma=1 tambien es un valor fijo, cont=0 valor fijo porque debe empezar a fuerzas a contar desde 0
 		
 		do {
 			fibonacci= suma;
--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 
 public class ForLoops {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner console = new Scanner (System.in);
+		System.out.println("Ingresa tu nombre:");
+		String name= console.next(); //Eso hace que ingrese cualquier nombre
+		printNameLength(name);
+		printNameCharacters(name);
+		console.close();
 	}
+		private static void printNameLength(String name) {
+		
+			System.out.println("Número total de caracteres:" + name.length());
+		
+	}
+		private static void printNameCharacters(String name) {
+		
+			int cantidad = String.valueOf(name).length();
+			for (int num=0; num<cantidad; num++) {
+				System.out.println(name.charAt(num)); //Letras que conforman el nombre
+			}//for
+		
+	}//main
 
-}
+	}//ForLoops
+
